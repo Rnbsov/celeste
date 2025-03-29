c1d65cc4a16065d25e8918b7e1e7adaea3bbf32c
rnbsovstudy <rnbsovstudy@gmail.com>
2025-03-29 12:20:40 +0000

    🎉 feat: Initalize auth pacakge
    Changelog: feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+
 import 'util.dart';
 import 'theme.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: 'https://jbpeszoljhnymgqhufmd.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpicGVzem9samhueW1ncWh1Zm1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyNTAzMDIsImV4cCI6MjA1ODgyNjMwMn0.NX3h_DpXqnpabbla3wR-RHQTuZskM3aJeX2qU1HNUe0',
+  );
   runApp(const MyApp());
 }
 
